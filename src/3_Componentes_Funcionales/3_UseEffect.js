4d44efb93b38784fa2c33ceb3fbe2a9ebca267a8
Juan Antonio <juanantonioamillopez@gmail.com>
2023-09-12 13:31:41 +0200

    Completar UseEffect()

--- src/3_Componentes_Funcionales/3_UseEffect.js
+++ src/3_Componentes_Funcionales/3_UseEffect.js
@@ -1,19 +1,71 @@
-import React, { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import text from '../translations/es/global.json';
 
-const UseEffect = () => {
+const Efecto1 = () => {
   useEffect(() => {
-    console.log('Soy un efecto');
+    console.log('Soy un efecto SIN DEPENDENCIA');
   });
-  
+
   useEffect(() => {
-    console.log('Soy otro efecto');
+    console.log('Soy otro efecto CON DEPENDENCIA');
   }, []);
 
   return (
     <div>
+      <h3>{text['functional-components-page-useeffect-effect1']}</h3>
+      <p>{text['functional-components-page-useeffect-message']}</p>
+    </div>
+  );
+};
+
+function Efecto2() {
+  const [valor, setValor] = useState(0);
+
+  useEffect(() => {
+    // Este código se ejecuta cada vez que 'valor' cambia
+    console.log('El valor ha cambiado:', valor);
+  }, [valor]); // El segundo argumento contiene 'valor' como dependencia
+
+  return (
+    <div>
+      <h3>{text['functional-components-page-useeffect-effect2']}</h3>
+      <p>
+        {text['functional-components-page-useeffect-effect2-value']} {valor}
+      </p>
+      <button onClick={() => setValor(valor + 1)}>{text['functional-components-page-useeffect-effect2-button']}</button>
+    </div>
+  );
+}
+
+function Efecto3() {
+  const [mostrar, setMostrar] = useState(true);
+
+  useEffect(() => {
+    // Este código se ejecuta cuando el componente se monta
+    console.log('Componente montado');
+
+    return () => {
+      // Este código se ejecuta cuando el componente se desmonta
+      console.log('Componente desmontado');
+    };
+  }, []);
+
+  return (
+    <div>
+      <h3>{text['functional-components-page-useeffect-effect3']}</h3>
+      <button onClick={() => setMostrar(!mostrar)}>{mostrar ? 'Ocultar' : 'Mostrar'}</button>
+      {mostrar && <p>{text['functional-components-page-useeffect-effect3-p']}</p>}
+    </div>
+  );
+}
+
+const UseEffect = () => {
+  return (
+    <div>
       <h2>{text['functional-components-page-useeffect']}</h2>
-      <p>{text['functional-components-page-message']}</p>
+      <Efecto1 />
+      <Efecto2 />
+      <Efecto3 />
     </div>
   );
 };
