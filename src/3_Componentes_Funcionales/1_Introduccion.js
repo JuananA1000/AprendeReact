ff7b245e0c2cda4da9d1df454bae4227e5cb7143
Juan Antonio <juanantonioamillopez@gmail.com>
2023-09-12 12:34:19 +0200

    Añadir modo noche

--- src/3_Componentes_Funcionales/1_Introduccion.js
+++ src/3_Componentes_Funcionales/1_Introduccion.js
@@ -1,13 +1,13 @@
-import React from 'react';
 import text from '../translations/es/global.json';
 
 const MiComponente = ({ unaProp, otraProp }) => {
   return (
     <div>
       <h2>{text['functional-components-page-intro']}</h2>
-      <p>{text['functional-components-page-phrase']}</p>
-      {text['functional-components-page-greetings1']} {unaProp}
-      {text['functional-components-page-greetings2']} {otraProp}
+      <p>{text['functional-components-page-intro-phrase']}</p>
+      {text['functional-components-page-intro-greetings1']} {unaProp}
+      <br />
+      {text['functional-components-page-intro-greetings2']} {otraProp}
     </div>
   );
 };
@@ -16,7 +16,12 @@ export default function Introduccion() {
   return (
     <div>
       <h1>{text['functional-components-page-title']}</h1>
-      <MiComponente unaProp={'Hola Mundo '} otraProp={'Adiós Mundo'} />
+
+      <MiComponente
+        // Podemos NO mostrar estas props si no nos interesa, o mostrar solo una de ellas
+        unaProp={'Hola Mundo '}
+        otraProp={'Adiós Mundo'}
+      />
     </div>
   );
 }
--- src/3_Componentes_Funcionales/2_UseState.js
+++ src/3_Componentes_Funcionales/2_UseState.js
@@ -4,11 +4,11 @@ import text from '../translations/es/global.json';
 
 import '../style/style.css';
 /**
- * useState() sirve para cxontrolar el estado de un componente, y ademas cambiarlo.
- * En este fichero, en el futuro, desarrollaremos, en iuna misma interfaz dos maneras de explicar este hook:
- * 
+ * useState() sirve para controlar el estado de un componente, y ademas cambiarlo.
+ * En este fichero, en el futuro, desarrollaremos, en una misma interfaz dos maneras de explicar este hook:
+ *
  * - La primera tendrú un botón que encienda y apague una bombilla.
- * 
+ *
  * - La segunda tendrú un botón que cuente de uno en uno.
  */
 
@@ -16,6 +16,8 @@ const UseState = () => {
   const [contador, setContador] = useState(0);
   const [items, setItems] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [luz, setLuz] = useState('☀️');
+  const [claseDiv, setClaseDiv] = useState('a');
 
   const agregarItem = () => {
     if (inputValue.trim() !== '') {
@@ -24,24 +26,46 @@ const UseState = () => {
     }
   };
 
+  const modoDia = () => {
+    setLuz('☀️');
+    setClaseDiv('a');
+
+    console.log('Ahora es de día');
+  };
+
+  const modoNoche = () => {
+    setLuz('🌙');
+    setClaseDiv('b');
+
+    console.log('Ahora es de noche');
+  };
+
   return (
     <div>
       <h2> {text['functional-components-page-usestate']} </h2>
+
+      <div className={claseDiv}>
+        <h3> {text['functional-components-page-usestate-switch']} </h3>
+        <div>{luz}</div>
+        <button onClick={modoDia}>{text['functional-components-page-usestate-switch-on']}</button>
+        <button onClick={modoNoche}>{text['functional-components-page-usestate-switch-off']}</button>
+      </div>
+
       <div className='a'>
-        <p>
-          {text['functional-components-page-usestate-counter']} {contador}
-        </p>
-        <button onClick={() => setContador(contador + 1)}>
-          {text['functional-components-page-usestate-counter+']}
-        </button>
+        <h3>{text['functional-components-page-usestate-counter']}</h3>
+        <div>{contador}</div>
         <button onClick={() => setContador(contador - 1)}>
           {text['functional-components-page-usestate-counter-']}
+        </button>
+        <button onClick={() => setContador(contador + 1)}>
+          {text['functional-components-page-usestate-counter+']}
         </button>
       </div>
 
       <div className='a'>
-        <button onClick={agregarItem}>{text['functional-components-page-add-button']} </button>
+        <h3>{text['functional-components-page-usestate-add']}</h3>
         <input type='text' value={inputValue} onChange={(event) => setInputValue(event.target.value)} />
+        <button onClick={agregarItem}>{text['functional-components-page-usestate-add-button']} </button>
         <ul>
           {items.map((item, index) => (
             <li key={index}>{item}</li>
