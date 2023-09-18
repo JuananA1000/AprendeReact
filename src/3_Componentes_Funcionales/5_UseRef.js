db2681212fad489a1a8a4513033f34e6cd7e9e22
Juan Antonio <juanantonioamillopez@gmail.com>
2023-09-18 14:06:21 +0200

    Corregir useRef()

--- src/3_Componentes_Funcionales/5_UseRef.js
+++ src/3_Componentes_Funcionales/5_UseRef.js
@@ -1,29 +1,64 @@
-import React, { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 import text from '../translations/es/global.json';
 
+/**
+ * useRef() es una herramienta que permite interactuar con el DOM de manera sencilla y eficiente. A diferencia de otros
+ * hooks, useRef() no causa una re-renderización cuando cambia su valor. Es decir, que el valor de nuestras variables se
+ * actualizará, y podremos verlo en la consola, pero en el DOM, el cambio no se hará efectivo mientras que no
+ * refresquemos.
+ */
+
 const UseRef = () => {
-  const ref = useRef(); // Ref Básico
-  const contadorRef = useRef(0); // Guardar y acceder a un valor mutable
+  const refBasico = useRef();
+  const contadorRef = useRef(0);
+  const seconds = useRef(0);
 
   const click = () => {
-    console.log('REF: ', ref);
+    console.log('REF: ', refBasico.current);
   };
 
-  const incrementarContador = () => {
-    contadorRef.current++;
-    console.log('Contador:', contadorRef.current);
+  const increment = () => {
+    contadorRef.current += 1;
+    console.log(contadorRef.current);
   };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      seconds.current += 1;
+      console.log(`Tiempo transcurrido: ${seconds.current} segundos`);
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
 
   return (
-    <>
-      <h2 onClick={click} ref={ref}>
-      {text['functional-components-page-useref']}
-      </h2>
+    <div>
+      <h2>{text['functional-components-page-useref']}</h2>
 
       <div>
-        <button onClick={incrementarContador}> {text['functional-components-page-useref-add']}</button>
+        <h3>{text['functional-components-page-useref-basic']}</h3>
+        <p onClick={click} ref={refBasico} style={{ backgroundColor: 'red' }}>
+          {text['functional-components-page-useref-basic-p']}
+        </p>
       </div>
-    </>
+
+      <div>
+        <h3>{text['functional-components-page-useref-counter']}</h3>
+        <p> {contadorRef.current}</p>
+        <button onClick={increment}>{text['functional-components-page-useref-counter-increase-button']}</button>
+      </div>
+
+      <div>
+        <h3>{text['functional-components-page-useref-seccounter']}</h3>
+        {seconds.current}
+      </div>
+
+      {/* 
+        Crear un botón que refresque el navegador sin reiniciar la información
+      */}
+    </div>
   );
 };
 
