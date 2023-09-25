566258eb0ef415a935329195e9bb0ec81e54ea38
Juanan <juanantonioamillopez@gmail.com>
2023-09-25 09:14:26 +0200

    Añadir encabezado dinámico

--- src/2_Componentes_con_Clases/1_Introduccion.js
+++ src/2_Componentes_con_Clases/1_Introduccion.js
@@ -1,11 +1,11 @@
+import { H1t2 } from '../components/Headers';
 import { Component } from 'react';
-import text from '../translations/es/global.json';
 
 export class Introduccion extends Component {
   render() {
     return (
       <div>
-        <h1>{text['class-component-page-title']}</h1>
+        <H1t2 />
       </div>
     );
   }
