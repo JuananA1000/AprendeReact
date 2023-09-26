c921562dd225e98f1c2035e8b3e80d15cd4d5461
Juanan <juanantonioamillopez@gmail.com>
2023-09-26 12:13:41 +0200

    Añadir counterSlice

--- src/10_Redux/3_Mas_ejemplos.js
+++ src/10_Redux/3_Mas_ejemplos.js
@@ -1,24 +1,34 @@
 import { increment, decrement } from './store/counterSlice';
 import { useDispatch, useSelector } from 'react-redux';
+import { useState } from 'react';
+import { addTask, removeTask } from './store/tasksSlice';
+
 export default function MasEjemplos() {
   const dispatch = useDispatch();
-  // const count = useSelector((state) => state.counter.count);
+  const contador = useSelector((state) => state.counter.count);
+  // const tasks = useSelector((state) => state);
+  const [newTask, setNewTask] = useState('');
 
+  const handleAddTask = () => {
+    dispatch(addTask(newTask));
+    setNewTask('');
+  };
   return (
     <div>
       <h2>10.3. Más Ejemplos</h2>
 
-      <div>
+      <div style={{ backgroundColor: 'aquamarine' }}>
         <h3>Contador </h3>
+        <h3>{contador} </h3>
         <button onClick={() => dispatch(increment())}>Incrementar</button>
         <button onClick={() => dispatch(decrement())}>Decrementar</button>
       </div>
 
-      <div>
+      <div style={{ backgroundColor: 'aquamarine' }}>
         <h3>Lista de Tareas</h3>
       </div>
 
-      <div>
+      <div style={{ backgroundColor: 'aquamarine' }}>
         <h3>Cambiar Tema</h3>
       </div>
     </div>
