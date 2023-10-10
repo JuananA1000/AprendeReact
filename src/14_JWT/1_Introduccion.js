95e9407380997e267fa59971e04167e1d1330d25
Juanan <juanantonioamillopez@gmail.com>
2023-10-10 11:59:25 +0200

    AY DIOS MIO, QUE NO ME SALE LO DEL JWT, COJONES. ¡¡COJONES!! ¡¡¡¡DADME EXCLAMACIONES MÁS GRANDES!!!! ¡¡¡¡HOSTIARDOS Y COÑUSTIOS!!!!

--- src/14_JWT/1_Introduccion.js
+++ src/14_JWT/1_Introduccion.js
@@ -1,23 +1,24 @@
+import { H1t14 } from '../components/Headers';
+import text from '../translations/es/global.json';
+
+/* 
+  Los JSON Web Tokens son objetos JSON que permiten a un usuario identificarse en una web y acceder a ciertas rutas
+  que, de lo contrario, le estarían denegadas. 
+
+  Código Mentor: Tutorial Básico
+  TUTORIAL: https://www.youtube.com/watch?v=cL3bXzUBFUA
+
+  Instalamos, en primer lugar express y JWT:
+  npm install react-jwt
+  npm install jsonwebtoken
+*/
+
 export default function Introduccion() {
-  /* 
-    Los JSON Web Tokens son objetos JSON que permiten a un usuario identificarse en una web y acceder a ciertas rutas
-    que, de lo contrario, le estarían denegadas. 
-
-    Código Mentor: Tutorial Básico
-    TUTORIAL: https://www.youtube.com/watch?v=cL3bXzUBFUA
-    
-    Fazt Code: Tutorial Opcional
-    TUTORIAL: https://www.youtube.com/watch?v=qckBlIfOnlA
-  */
-
   return (
     <div>
-      <h1>14. JSON Web Tokens (JWT)</h1>
-      <h2>Introducción</h2>
-      <p>
-        Los JSON Web Tokens son objetos JSON que permiten a un usuario identificarse en una web y acceder a ciertas
-        rutas que, de lo contrario, le estarían denegadas.
-      </p>
+      <H1t14 />
+      <p>{text['jwt-page-p']}</p>
+      <h2>{text['jwt-page-intro']}</h2>
     </div>
   );
 }
