522c8d3a720e01c9927fc50db11e937133ae6716
Juanan <juanantonioamillopez@gmail.com>
2023-10-18 12:45:37 +0200

    Drag & Drop

--- src/14_Drag_and_Drop/1_Introduccion.js
+++ src/14_Drag_and_Drop/1_Introduccion.js
@@ -1,10 +1,54 @@
 import { H1t14 } from '../components/Headers/Headers';
+import User from './components/User';
+import { useState } from 'react';
+import text from '../translations/es/global.json';
+import { DndContext, closestCenter } from '@dnd-kit/core';
+import { SortableContext, arrayMove, verticalListSortingStrategy } from '@dnd-kit/sortable';
+
+// TUTORIAL: https://youtu.be/swFjfjLXe3I?si=3gfVwwLfHyVx8IsB
 
 export default function Introduccion() {
+  const [people, setPeople] = useState([
+    { id: 1, name: 'JuANAN' },
+    { id: 2, name: 'Koki' },
+    { id: 3, name: 'Junior' },
+    { id: 4, name: 'Walter' },
+    { id: 5, name: 'Landa' },
+    { id: 6, name: 'Albert' },
+    { id: 12, name: 'Usurero12' },
+  ]);
+
+  const handleDragEnd = (event) => {
+    console.log('Estado INICIAL del array: ', people);
+
+    const { active, over } = event;
+
+    console.log('Active ID: ', active.id);
+    console.log('Over ID: ', over.id);
+
+    const oldIndex = people.findIndex((person) => person.id === active.id);
+    const newIndex = people.findIndex((person) => person.id === over.id);
+
+    console.log('Old index: ', oldIndex);
+    console.log('New index: ', newIndex);
+
+    const finalPeople = arrayMove(people, oldIndex, newIndex);
+    setPeople(finalPeople);
+
+    console.log('Estado FINAL del array: ', finalPeople);
+  };
+
   return (
-    <div>
+    <DndContext collisionDetection={closestCenter} onDragEnd={handleDragEnd}>
       <H1t14 />
-    
-    </div>
+      <h2>{text['dnd-page-intro']}</h2>
+      <div style={{ width: '40%', marginTop: '20px' }}>
+        <SortableContext items={people} strategy={verticalListSortingStrategy}>
+          {people.map((user) => (
+            <User user={user} key={user.id} />
+          ))}
+        </SortableContext>
+      </div>
+    </DndContext>
   );
 }
---
+++ src/14_Drag_and_Drop/components/User.jsx
@@ -0,0 +1,28 @@
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
+
+export default function User({ user }) {
+  const { attributes, listeners, setNodeRef, transform, transition } = useSortable({
+    // useSortable tiene una propiedad 'id' la cual podemos utilizar para ordenar nuestras listas
+    id: user.id,
+  });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
+
+  return (
+    <div {...attributes} {...listeners} ref={setNodeRef} style={style}>
+      <h3
+        style={{
+          background: 'linear-gradient(180deg, #4ed61e 0%, #688800 99.99%, rgba(57, 82, 48, 0) 100%)',
+          padding: '30px',
+          borderRadius: '10px',
+          marginBottom: '10px',
+        }}>
+        {user.name}
+      </h3>
+    </div>
+  );
+}
